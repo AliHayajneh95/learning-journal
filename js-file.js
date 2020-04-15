29f162fd8930c13c5cd5a01f298ce176ef35a4ef
AliHayajneh95 <Hayajnehali95@gmail.com>
2020-04-15 12:12:28 +0300

    completed this task

--- js-file.js
+++ js-file.js
@@ -1,3 +1,19 @@
+'use strict'
+window.prompt("enter your age", "man you are so old, deal with it");
+var a = prompt('1+1 = ?');
+var a2 = prompt('What is your favorite movie?');
+var a3 = prompt('Type your first 3 names');
+var res ;
+if (a == 2){
+    res = 'you are smart good';
+}
+else {
+    res = 'wrong try again';
+}
+document.write('<p id="a2">'+res+'</p>');
+document.write('<p id="a2">'+a2+'</p>');
+document.write('<p id="a3">'+a3+'</p>');
+
 function myFunction() {
     var person = prompt("Please enter your name", "");
     if (person != null) {
